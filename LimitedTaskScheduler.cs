668443e1afc08aa294bb3e749908118d77db52cb
Александр Гвоздев <gvozdev-work@mail.ru>
2019-05-26 18:51:08 +0700

    Initial commit

---
+++ LimitedTaskScheduler.cs
@@ -0,0 +1,14 @@
+namespace TaskLimitor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class LimitedTaskScheduler
+    {
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaskLimitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
